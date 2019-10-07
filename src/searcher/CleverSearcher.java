b55167c80920641437f30039615e03a7b89ea79b
u1860830 <salvatoretrn@gmail.com>
2019-10-07 15:12:53 +0100

    Commit 2

--- src/searcher/CleverSearcher.java
+++ src/searcher/CleverSearcher.java
@@ -1,7 +1,5 @@
 package searcher;
 
-import java.util.Arrays;
-
 public class CleverSearcher extends Searcher {
     CleverSearcher(int[] array, int k) {
         super(array, k);
@@ -9,12 +7,17 @@ public class CleverSearcher extends Searcher {
 
     public int findElement() throws IndexingError {
         int[] array = getArray();
-        int k = array[0];
+        int k = array[0]; // set kth element equal to the first element of the array
 
         for(int i = 0; i <= getArray().length - 1; i++){
             if(k <= 0 || k > array.length){
                 throw new IndexingError();
-            }if (array[i] > k) {
+
+                /**
+                 * Compare each element in the array (array[i]) with k,
+                 * if it's large than the
+                 */
+            }if (array[i] > k) { // compare the element i in the array to k
                 k = array[i];
             }
         } return k;
